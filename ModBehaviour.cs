d190dcdfdc9d5c1e9282eb0008ddd70833a9ef95
rod.xi <rod.xi@headwin.com.cn>
2025-11-10 09:04:06 +0800

    精简监听代码启用显示高价值物品的代码

--- ModBehaviour.cs
+++ ModBehaviour.cs
@@ -29,7 +29,7 @@ namespace LikeItemFind
         private HashSet<GameObject> _questCircleObjects = new HashSet<GameObject>();
         public InteractableLootbox[] AllLootboxesCache;
         public  InteractablePickup[] AllPickupsCache;
-        public bool ShowHighValueItem = false;
+        public bool IsShowHighValueItem = false;
 
         void Awake()
         {
@@ -56,22 +56,14 @@ namespace LikeItemFind
         {
         }
         // 不知道为什么我用这个方法电脑特别卡，暂时删除了
-        //void Update()
-        //{
-        //    // 按下pageUpKey展示高价值物品
-        //    if (Keyboard.current.pageUpKey.wasReleasedThisFrame)
-        //    {
-        //        ShowHighValueItem = true;
-        //        Debug.Log($"按下pageup1.3");
-        //    }
-        //    // 按下pageDownKey隐藏高价值物品
-        //    if (Keyboard.current.pageDownKey.wasReleasedThisFrame)
-        //    {
-        //        ShowHighValueItem = false;
-        //        Debug.Log($"按下pageup1.3");
-        //    }
-            
-        //}
+        void Update()
+        {
+            //// 按下LeftAlt切换展示高价值物品
+            if (Input.GetKeyUp(KeyCode.LeftAlt))
+            {
+                IsShowHighValueItem = !IsShowHighValueItem;
+            }
+        }
         // 场景加载完以后扫描地图上掉落的东西，根据手动标记，是否显示地图上的位置
         private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
         {
@@ -139,7 +131,7 @@ namespace LikeItemFind
             foreach (var pickup in AllPickupsCache)
             {
                 if (ItemWishlist.GetWishlistInfo(pickup.ItemAgent.Item.TypeID).isManuallyWishlisted
-                    ||(ShowHighValueItem && pickup.ItemAgent.Item.Value>10000))
+                    ||(IsShowHighValueItem && pickup.ItemAgent.Item.Value>10000))
                 {
                     DrawQuestMarker(pickup.ItemAgent.transform.position, 10f, pickup.ItemAgent.Item.DisplayName);
                     circlesDrawn++;
@@ -154,11 +146,11 @@ namespace LikeItemFind
                 // 排除玩家仓库跟宠物背包
                 if (boxName.IndexOf("PetProxy") < 0 && boxName.IndexOf("PlayerStorage") < 0)
                 {
-                    Debug.Log($"箱子方法里的ShowHighValueItem={ShowHighValueItem}");
+                    Debug.Log($"箱子方法里的ShowHighValueItem={IsShowHighValueItem}");
                     // 筛选被标记物品的箱子，或者有价值高的物品的箱子
                     var isManuallyWishlistedList = lootbox.Inventory.Content
                         .Where(x => x != null && ItemWishlist.GetWishlistInfo(x.TypeID).isManuallyWishlisted
-                                    || (ShowHighValueItem && x.Value > 10000));
+                                    || (IsShowHighValueItem && x.Value > 10000));
 
                     // 有被标记物品的箱子跟有价值高物品的箱子就在地图上绘制出来
 
@@ -224,21 +216,17 @@ namespace LikeItemFind
         // 获取图标方法
         private Sprite GetQuestIcon()
         {
-
             List<Sprite> allIcons = MapMarkerManager.Icons;
-
             if (allIcons == null)
             {
                 Debug.LogError(_logPrefix + "MapMarkerManager.Icons 列表为 null! 无法获取图标。");
                 return null;
             }
-
             if (allIcons.Count > 0)
             {
                 Debug.LogWarning(_logPrefix + "使用列表中的第一个图标作为后备。");
                 return allIcons[0];
             }
-
             Debug.LogError(_logPrefix + "MapMarkerManager.Icons 列表为空! 没有任何图标。");
             return null;
         }
@@ -258,12 +246,10 @@ namespace LikeItemFind
         private bool IsMapOpen()
         {
             MiniMapView view = MiniMapView.Instance;
-
             if (view != null)
             {
                 return view == View.ActiveView;
             }
-
             return false;
         }
         
